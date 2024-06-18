8a76b42567baafbcfb626a341f3ff0fc1a8687f9
abhishek kumar <abhiv5976@gmail.com>
2024-06-18 23:13:13 +0530

    changes something
    hello
    
    Co-Authored-By: Rishabh Singh Satya <80286651+rishabhsinghsatya@users.noreply.github.com>

--- assigment/19/03/2024.py
+++ assigment/19/03/2024.py
@@ -1,12 +1,4 @@
-def missing_no(array, n):
-    summ=0
-    for i in (array):
-        summ+=i
-    a=n*(n+1)//2-summ
-    return a
-array=[1,2,3,9,8,7,6,4,10]
-n=10
-print(missing_no(array,n))
+
 
 
 def count_smaller_element(array,x):
@@ -99,3 +91,13 @@ if __name__ == '__main__':
  
     nums = [0, 0, 1, 0, 1, 0, 0]
     findLargestSublist(nums)
+    
+def missing_no(array, n):
+    summ=0
+    for i in (array):
+        summ+=i
+    a=n*(n+1)//2-summ
+    return a
+array=[1,2,3,9,8,7,6,4,10]
+n=10
+print(missing_no(array,n))
